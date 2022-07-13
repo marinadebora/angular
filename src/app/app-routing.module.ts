7f675387f836f3e6486b0b652eb6bc988e753e8f
Debora <m_deby_c@hotmail.com>
2022-07-12 22:24:20 -0300

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearProductosComponent } from './components/crear-productos/crear-productos.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: ListarProductosComponent },
+  { path:'crear-producto', component: CrearProductosComponent },
+  { path:'editar-producto/:id', component: CrearProductosComponent },
+  { path:'**', redirectTo:'', pathMatch:'full'}//esta linea evita que se rompa si paso url cualquiera, redirecciona a a ruta principal
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrearProductosComponent } from './components/crear-productos/crear-productos.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductosComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
